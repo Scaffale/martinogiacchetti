14f2589d741600b8319415a06f33f3f19fadae08
Martino <martino.giacchetti@gmail.com>
2020-06-13 15:19:25 +0200

    working on single page

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,31 @@
 function initGsap() {
   gsap.registerPlugin(ScrollTrigger);
-  let tl = gsap.timeline({
-    // yes, we can add it to an entire timeline!
-    scrollTrigger: {
-      // trigger: ".container",
-      // pin: true,   // pin the trigger element while active
-      // start: "top top", // when the top of the trigger hits the top of the viewport
-      // end: "+=500", // end after scrolling 500px beyond the start
-      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-      snap: {
-        snapTo: "labels", // snap to the closest label in the timeline
-        duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-        ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-      }
-    }
-  });
+  let tl = gsap.timeline();
 
   // add animations and labels to the timeline
-  tl.addLabel("start")
-    .to('.scroll', { opacity: 0 })
-    .from(".name .char", { rotateY: '.25turn', scale: 0.9, opacity: 0, stagger: {
-                                                                  grid: 'auto',
-                                                                  from: "random",
-                                                                  amount: 0.3 }
+  tl.addLabel('start')
+    .from('.name .char', { rotateY: '.25turn', scale: 0.9, opacity: 0, stagger: {
+                                                                                  grid: 'auto',
+                                                                                  from: 'random',
+                                                                                  amount: 0.3 }
       })
-    .addLabel("nameMoving")
-    // .to(".name .char", { y: '-100vh', stagger: {
-    //                                 grid: 'auto',
-    //                                 from: "random",
-    //                                 amount: 0.3 }
-    //   })
-    // .addLabel("imageIn")
-    .to(".face", { scale: 1, opacity: 1 })
-    .addLabel("menuIn")
-    .from(".menu", { opacity: 0 })
-    .addLabel("end");
+    .addLabel('what')
+    .from('.frontend .char', { x: -50, opacity: 0, stagger: {
+                                                              grid: 'auto',
+                                                              from: 'random',
+                                                              amount: 0.6 }
+      })
+    .from('.ux .char', { x: -50, opacity: 0, stagger: {
+                                                        grid: 'auto',
+                                                        from: 'random',
+                                                        amount: 0.6 }
+      })
+    .from('.dev .char', { x: -50, opacity: 0, stagger: {
+                                                        grid: 'auto',
+                                                        from: 'random',
+                                                        amount: 0.6 }
+      })
+    .addLabel('menuIn')
+    .from('.menu', { opacity: 0 })
+    .addLabel('end');
 }
