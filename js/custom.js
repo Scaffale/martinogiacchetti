1b13f77797bb564d95d19724c4f37114463475e3
Martino <martino.giacchetti@gmail.com>
2020-06-17 16:01:24 +0200

    giving some shape

--- js/custom.js
+++ js/custom.js
@@ -9,22 +9,22 @@ function initGsap() {
     .to('.greetings', { opacity: 0, duration: 0.3, delay: 1 })
     .from('.iam', { opacity: 0, duration: 1 })
     .addLabel('name-in')
+    .to('.iam', { opacity: 0, duration: 0.3 })
     .from('.name .char', { rotateY: '.25turn',
                            scale: 0.9,
                            opacity: 0,
-                           delay: 0.6,
+                           delay: 0,
                            duration: 1,
                            stagger: { grid: 'auto', from: 'center', amount: 0.3 } })
     .addLabel('what');
   document.querySelectorAll('.what div').forEach(function(element, index) {
     tl.from('.what div:nth-of-type(' + (index + 1) + ') .char', { x: -50,
                                                             opacity: 0,
+                                                            delay: -0.2,
                                                             duration: 0.3,
                                                             stagger: {grid: 'auto', from: 'random', amount: 0.3 } })
   });
-  tl.addLabel('what-shado')
-    .to('.what div', { className: 'shadows', stagger: { grid: 'auto', from: 0, amount: 0.2 } })
-    .addLabel('end');
+  tl.addLabel('end');
 }
 
 function randomColor() {
